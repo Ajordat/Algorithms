db70675a20e4d92fd58bd3e6c08a826a03c0711f
Ajordat <alex.jorda@blackdata.systems>
2019-10-11 11:52:05 +0200

    Added DirectGraph testing.

--- test/models/test_graphs.py
+++ test/models/test_graphs.py
@@ -1,5 +1,6 @@
 import unittest
 
+from src.models.graphs.directed_graph import DirectedGraph
 from src.models.graphs.graph import Graph
 
 
@@ -28,7 +29,23 @@ class TestGraph(unittest.TestCase):
 
         self.assertRaises(KeyError, g.add_edge, 'A', 'X')
         self.assertRaises(KeyError, g.add_edge, 'X', 'B')
-        self.assertRaises(KeyError, g.add_edge, 'X', 'X')
+        self.assertRaises(KeyError, g.add_edge, 'X', 'Y')
         self.assertIn('B', g.get_edges('A'))
         self.assertIn('A', g.get_edges('B'))
+        self.assertRaises(KeyError, g.get_edges, 'X')
+
+
+class TestDirectedGraph(unittest.TestCase):
+
+    def test_add_edge(self):
+        g = DirectedGraph()
+        g.add_node('A')
+        g.add_node('B')
+        g.add_edge('A', 'B')
+
+        self.assertRaises(KeyError, g.add_edge, 'A', 'X')
+        self.assertRaises(KeyError, g.add_edge, 'X', 'B')
+        self.assertRaises(KeyError, g.add_edge, 'X', 'Y')
+        self.assertIn('B', g.get_edges('A'))
+        self.assertNotIn('A', g.get_edges('B'))
         self.assertRaises(KeyError, g.get_edges, 'X')
